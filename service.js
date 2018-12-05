1c7691a50b037b970fd48f66ca3de9953bf42484
陈国涛 <969870539@qq.com>
2018-12-05 15:27:51 +0800

    修改db

--- service.js
+++ service.js
@@ -11,60 +11,101 @@ function keysort(key,sortType){
 exports.showIndex = (req,res) => {
   let sql = 'select * from book';
   let data = null;
-
-  db.base(sql, data, (result)=>{
+  // db.base(sql, data, (result)=>{
+  //   result.sort(keysort('id',false));
+  //   res.json(result);
+  // })
+  const connection = db.connection();
+  db.base(connection, sql, data, (result) => {
     result.sort(keysort('id',false));
     res.json(result);
-  })
+  });
+  db.close(connection);
+  return;
 }
 
 // 添加数据
 exports.addBook = (req,res) => {
   const info = req.body;
   let sql = 'insert into book set ?';
-
-  db.base(sql, info, (result)=>{
+  // db.base(sql, info, (result)=>{
+  //   if(result.affectedRows == 1){
+  //     res.json({flag : 1});
+  //   }else{
+  //     res.json({flag : 2});
+  //   }  
+  // })
+  const connection = db.connection();
+  db.base(connection, sql, info, (result) => {
     if(result.affectedRows == 1){
       res.json({flag : 1});
     }else{
       res.json({flag : 2});
     }  
-  })
+  });
+  db.close(connection);
+  return;
 }
 
 exports.getBookById = (req,res) => {
   let id = req.params.id;
   let sql = 'select * from book where id=?';
   let data = [id];
-  db.base(sql,data,(result)=>{
-      res.json(result[0]);
+  // db.base(sql,data,(result)=>{
+  //     res.json(result[0]);
+  // });
+  const connection = db.connection();
+  db.base(connection, sql, data, (result) => {
+    res.json(result[0]);
   });
+  db.close(connection);
+  return;
 };
 
 exports.editBook = (req,res) => {
   let info = req.body;
   let sql = 'update book set name=?,author=?,category=?,description=? where id=?';
   let data = [info.name,info.author,info.category,info.description,info.id];
-  db.base(sql,data,(result)=>{
-      if(result.affectedRows == 1){
-          res.json({flag : 1});
-      }else{
-          res.json({flag : 2});
-      }  
+  // db.base(sql,data,(result)=>{
+  //     if(result.affectedRows == 1){
+  //         res.json({flag : 1});
+  //     }else{
+  //         res.json({flag : 2});
+  //     }  
+  // });
+  const connection = db.connection();
+  db.base(connection, sql, data, (result) => {
+    if(result.affectedRows == 1){
+      res.json({flag : 1});
+    }else{
+      res.json({flag : 2});
+    }  
   });
+  db.close(connection);
+  return;
 };
 
 exports.deleteBook = (req,res) => {
   let id = req.params.id;
   let sql = 'delete from book where id=?';
   let data = [id];
-  db.base(sql,data,(result)=>{
-      if(result.affectedRows == 1){
-          res.json({flag : 1});
-      }else{
-          res.json({flag : 2});
-      } 
+  // db.base(sql,data,(result)=>{
+  //     if(result.affectedRows == 1){
+  //         res.json({flag : 1});
+  //     }else{
+  //         res.json({flag : 2});
+  //     } 
+  // });
+  const connection = db.connection();
+  db.base(connection, sql, data, (result) => {
+    if(result.affectedRows == 1){
+      res.json({flag : 1});
+    }else{
+      res.json({flag : 2});
+    }  
   });
+  db.close(connection);
+  return;
 };
 
 
